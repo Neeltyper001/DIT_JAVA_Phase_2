f8c9f80df4f11ef224db92ad57f08b96910c491b
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-06-06 20:28:56 +0530

    updated

--- NestedRecursion/Pattern.java
+++ NestedRecursion/Pattern.java
@@ -1,5 +1,22 @@
 package NestedRecursion;
 
-public class patter {
-    
+ class Pattern {
+    static void printStar(int stars){
+        if(stars  == 0){
+            return;
+        }
+        
+        printStar(stars - 1);
+        System.out.println("*");
+        return;
+    }
+
+    static void printLine(int rows, int stars){
+        if(rows == 0){
+            return;
+        }
+
+        printLine(rows - 1, stars);
+        printStar(stars);
+    }
 }
