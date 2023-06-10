7904a4d8be501ed64fd5ffd808863918938f3464
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-06-10 23:50:41 +0530

    updated all interformat conversions

--- InterFormatConversion/DecimalToBinary.java
+++ InterFormatConversion/DecimalToBinary.java
@@ -15,6 +15,6 @@ public class DecimalToBinary {
     }
 
     public static void main(String[] args) {
-        System.out.println(decimalToBinary(7));
+        System.out.println(decimalToBinary(14));
     }
 }
